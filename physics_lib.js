6e46c4830b7ccc658339a0b501c5db77e5eaac3a
Sam Brunacini <samlbrunacini@gmail.com>
2021-05-19 15:51:00 -0400

    Update projectile motion simulation and physics_lib

--- physics_lib.js
+++ physics_lib.js
@@ -7,6 +7,12 @@ const constants = {
 };
 
 
+function nearestGreaterMagnitudeInteger(x) {
+  if (x > 0) return Math.ceil(x);
+  return Math.floor(x);
+}
+
+
 class BaseSimulation {
 
   /*
@@ -21,11 +27,13 @@ class BaseSimulation {
     timeStep: time step per interation of the simulation
     */
     this.setTimeStep(timeStep);
-    this.setPixelsPerMeter(pixelsPerMeter);
     this.setScales(canvasWidth, canvasHeight);
+    this.setPixelsPerMeter(pixelsPerMeter);
 
     this.simTime = 0;
     this.paused = false;
+    this.gridlines = false;
+    this.showSimTime = true;
     this.physicsObjects = [];
   }
 
@@ -35,14 +43,21 @@ class BaseSimulation {
   }
 
   setPixelsPerMeter(pixelsPerMeter) {
-    /* Set a new pixels to meters ratio */
+    /* Set a new pixels to meters ratio and computes number of units in each direction */
     this.pixelsPerMeter = pixelsPerMeter;
+    this.xUnits = this.canvasWidth / this.pixelsPerMeter;
+    this.yUnits = this.canvasHeight / this.pixelsPerMeter;
   }
 
   setScales(canvasWidth, canvasHeight) {
     /* Determines the pixel to meter scale when the canvas resizes */
     this.canvasWidth = canvasWidth;
     this.canvasHeight = canvasHeight;
+  }
+
+  resetSimulationTime() {
+    /* Set the simulation time to zero */
+    this.simTime = 0;
   }
 
   pause() {
@@ -53,6 +68,16 @@ class BaseSimulation {
   unpause() {
     /* Pause the simulation */
     this.paused = true;
+  }
+
+  toggleGridlines() {
+    /* Toggle whether gridlines are drawn to the screen */
+    this.gridlines = !this.gridlines;
+  }
+
+  toggleShowSimulationTime() {
+    /* Toggle whether the simulation time is drawn to the screen */
+    this.showSimTime = !this.showSimTime;
   }
 
   addPhysicsObject(physicsObject) {
@@ -70,14 +95,43 @@ class BaseSimulation {
     return quantity * this.pixelsPerMeter;
   }
 
+  drawGridlines() {
+    push();
+    strokeWeight(1);
+    stroke(128, 128);
+    let point1, point2;
+    for (let x=0; x<this.xUnits+1; x++) {
+      point1 = this.transformCoordinates(createVector(x, 0));
+      point2 = this.transformCoordinates(createVector(x, this.yUnits));
+      line(point1.x, point1.y, point2.x, point2.y);
+    }
+    for (let y=0; y<this.yUnits+1; y++) {
+      point1 = this.transformCoordinates(createVector(0, y));
+      point2 = this.transformCoordinates(createVector(this.xUnits, y));
+      line(point1.x, point1.y, point2.x, point2.y);
+    }
+    pop();
+  }
+
+  drawSimulationTime() {
+    push();
+    noStroke();
+    fill(0);
+    textSize(20);
+    text("Simulation time: " + Math.round(this.simTime*10)/10, 20, 30);
+    pop();
+  }
+
   update() {
     /* Do a single simulation step */
     if (!this.paused) {
       clear();
+      if (this.gridlines) this.drawGridlines();
       for (let obj of this.physicsObjects) {
         obj.update();
       }
       this.simTime += this.timeStep;
+      if (this.showSimTime) this.drawSimulationTime();
     }
   }
 
@@ -94,9 +148,8 @@ class PhysicsObject {
   constructor(sim, x, y, mass=1) {
     this.sim = sim;
     this.sim.addPhysicsObject(this);
-    this.pos = createVector(x, y);
-    this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
+    this.originalPosition = createVector(x, y);
+    this.reset();
     this.mass = mass;
     this.inverseMass = 1/this.mass;
   }
@@ -105,11 +158,26 @@ class PhysicsObject {
     this.pos = pos;
   }
 
+  setVelocity(vel) {
+    this.vel = vel;
+  }
+
+  setAcceleration(acc) {
+    this.acc = acc;
+  }
+
   applyForce(force) {
     this.acc.add(p5.Vector.mult(force, this.inverseMass));
   }
 
+  reset() {
+    this.pos = this.originalPosition.copy();
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+  }
+
   update() {
+    this.vel.y = min(this.vel.y, 1000);
     this.vel.add(p5.Vector.mult(this.acc, this.sim.timeStep));
     this.pos.add(p5.Vector.mult(this.vel, this.sim.timeStep));
     this.acc.set(0, 0, 0);
--- projectile_motion_driver.js
+++ projectile_motion_driver.js
@@ -3,8 +3,25 @@
 
 class ProjectileMotionSimulation extends BaseSimulation {
 
+	/* Simulate projectile motion of an object in freefall */
+
 	constructor(canvasWidth, canvasHeight, pixelsPerMeter=1, timeStep=1/60) {
 		super(canvasWidth, canvasHeight, pixelsPerMeter, timeStep);
+	}
+
+	positionAtTime(initialPos, initialVel, t) {
+		/* Get the position of the an object with given initial position and initial velocity at time t */
+		return createVector(initialPos.x + initialVel.x * t, initialPos.y + initialVel.y * t - 0.5 * constants.g * t * t);
+	}
+
+	peakTime(initialVel) {
+		/* Get the time where an object with the given initial velocity will be at its peak height */
+		return initialVel.y / constants.g;
+	}
+
+	peakHeight(initialPos, initialVel) {
+		/* Get the maximum height of an object with given initial position and initial velocity */
+		return this.positionAtTime(initialPos, initialVel, this.peakTime(initialPos, initialVel));
 	}
 
 }
@@ -12,14 +29,23 @@ class ProjectileMotionSimulation extends BaseSimulation {
 
 class Projectile extends PhysicsObject {
 
+	/* The object of the projectile motion simulation */
+
 	constructor(sim, x, y, radius, col, mass=1) {
 		super(sim, x, y, mass);
 		this.radius = radius;
 		this.diameter = 2 * this.radius;
 		this.col = col;
+		this.setVelocity(createVector(6, 5));
+	}
+
+	lockBounds() {
+		/* Keep the projectile on screen */
+		this.pos.set(constrain(this.pos.x, this.radius, this.sim.xUnits-this.radius), constrain(this.pos.y, this.radius, this.sim.yUnits-this.radius));
 	}
 
 	draw() {
+		/* Draw the projectile */
 		const coords = this.sim.transformCoordinates(this.pos);
 		push();
 		fill(this.col.x, this.col.y, this.col.z);
@@ -28,7 +54,9 @@ class Projectile extends PhysicsObject {
 	}
 
 	update() {
+		this.applyForce(createVector(0, -constants.g));
 		super.update()
+		this.lockBounds();
 		this.draw();
 	}
 
@@ -36,10 +64,11 @@ class Projectile extends PhysicsObject {
 
 let simulation, projectile;
 function setup() {
-	const canvas = createCanvas(windowWidth, windowHeight);
+	const canvas = createCanvas(windowWidth/2, windowHeight);
 	canvas.parent("canvas-div");
-	simulation = new ProjectileMotionSimulation(width, height, 15);
-	projectile = new Projectile(simulation, 10, 10, 2, createVector(0,255,0));
+	simulation = new ProjectileMotionSimulation(width, height, 50);
+	simulation.toggleGridlines();
+	projectile = new Projectile(simulation, 2, 10, 0.25, createVector(255,0,0));
 }
 
 function draw() {
